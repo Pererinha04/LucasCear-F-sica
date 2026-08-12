2858681205061105fdfa9e5cfb90854b81c1bbe3
Pererinha04 <pererinhaa04022@gmail.com>
2026-08-11 21:58:52 -0300

    Create home.js

---
+++ home.js
@@ -0,0 +1,17 @@
+//------ Button "Ver aulas" ------
+
+
+const btaulas = document.getElementById("btaulas")
+
+btaulas.addEventListener("click", () => {
+    window.location.href = "index2.html";
+})
+
+
+//------ Button "Atividades" ------
+
+const btatividades = document.getElementById("btatividades")
+
+btatividades.addEventListener("click", () => {
+    window.location.href = "index3.html"
+})
